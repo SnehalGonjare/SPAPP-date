db4974d799dfbb9beb2106496daded84959d0d3b
unknown <162harshalsurve@gmail.com>
2018-02-07 21:55:53 +0530

    design change

--- controller/landingController.js
+++ controller/landingController.js
@@ -22,12 +22,14 @@ myApp.controller("landingCtrl", function ($scope) {
 
     // Get start date and End date on change date filter helper option
     $scope.getDates = function (dateFilterHelper) {
+        $scope.showData = true;
+
         $scope.date = new Date(); // get current date
-        // $scope.date = $scope.date.setDate($scope.date.getDate() + 1); // get current date
-        $scope.first = $scope.date.getDate() - $scope.date.getDay(); // First day is the day of the month - the day of the week
-        $scope.last = $scope.first + 6; // last day is the first day + 6
-        //    $scope.todayTimeStamp = +new Date; // Unix timestamp in milliseconds
-        //    $scope.oneDayTimeStamp = 1000 * 60 * 60 * 24; // Milliseconds in a day
+        // $scope.date = new Date('2018-02-16T03:24:00'); // ForUnit testing
+        // $scope.first = $scope.date.getDate() - $scope.date.getDay(); // First day is the day of the month - the day of the week
+        // $scope.last = $scope.first + 6; // last day is the first day + 6
+        // $scope.todayTimeStamp = +new Date; // Unix timestamp in milliseconds
+        // $scope.oneDayTimeStamp = 1000 * 60 * 60 * 24; // Milliseconds in a day
         switch (dateFilterHelper) {
             case 'Today':
                 $scope.startDate = $scope.date;
@@ -41,13 +43,24 @@ myApp.controller("landingCtrl", function ($scope) {
                 $scope.endDate = $scope.yesterdayDate;
                 break;
             case 'This Week':
-                $scope.startDate = new Date($scope.date.setDate($scope.first));
-                $scope.endDate = new Date($scope.date.setDate($scope.last));
-                break;
-            case 'Last Week':
+                // $scope.first = $scope.date.getDate() - $scope.date.getDay(); // First day is the day of the month - the day of the week
+                // $scope.last = $scope.first + 6; // last day is the first day + 6
+                // $scope.startDate = new Date($scope.date.setDate($scope.first));
+                // $scope.endDate = new Date($scope.date.setDate($scope.last));
+                // break;
+
+                $scope.first = $scope.date.getDate() - $scope.date.getDay(); // First day is the day of the month - the day of the week
+                // $scope.last = $scope.first + 6; // last day is the first day + 6
                 $scope.startDate1 = new Date($scope.date.setDate($scope.first));
                 $scope.startDate2 = angular.copy($scope.startDate1);
-                // $scope.endDate1 = new Date($scope.date.setDate($scope.last));
+                $scope.startDate = $scope.startDate1.setDate($scope.startDate1.getDate());
+                $scope.endDate = $scope.startDate2.setDate($scope.startDate2.getDate() + 6);
+                break;
+            case 'Last Week':
+                $scope.first = $scope.date.getDate() - $scope.date.getDay(); // First day is the day of the month - the day of the week
+                // $scope.last = $scope.first + 6; // last day is the first day + 6
+                $scope.startDate1 = new Date($scope.date.setDate($scope.first));
+                $scope.startDate2 = angular.copy($scope.startDate1);
                 $scope.startDate = $scope.startDate1.setDate($scope.startDate1.getDate() - 7);
                 $scope.endDate = $scope.startDate2.setDate($scope.startDate2.getDate() - 1);
                 break;
